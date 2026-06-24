eadc86466eb9944b8bddad45527543b7a094556f
venkatakk703 <venkatakk703@gmail.com>
2026-06-24 09:13:41 -0400

    added cors

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+    origin: 'http://localhost:3000' // your Next.js frontend
+}));
 
 app.use(express.json());
 
